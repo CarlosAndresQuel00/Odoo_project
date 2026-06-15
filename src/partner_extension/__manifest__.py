a9a7e50e6fe45d750a77e0b2254205eabee5c18b
Carlos Andres Quel <66259796+CarlosAndresQuel00@users.noreply.github.com>
2026-06-15 18:04:50 -0500

    Odoo project and PostgreSQL with Docker
    Add docker-compose.yml with Odoo.
    Add custom module in src.

---
+++ src/partner_extension/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Partner Extension',
+    'version': '1.0',
+    'summary': 'Extiende contactos con campos de riesgo y residencia',
+    'depends': ['base'],
+    'data': [
+        'views/partner_view.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
